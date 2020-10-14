5a840ac91f8be576a1d863c6dda7cf91f0920236
samyadeep <samyadeep.paul@quinta.co.in>
2020-10-14 20:44:34 +0530

    addaed all dependency in pom

--- innoventesofflinetest/src/main/java/com/innoventes/test/InnoventesofflinetestApplication.java
+++ innoventesofflinetest/src/main/java/com/innoventes/test/InnoventesofflinetestApplication.java
@@ -1,4 +1,4 @@
-package com.innoventes.test.innoventesofflinetest;
+package com.innoventes.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
