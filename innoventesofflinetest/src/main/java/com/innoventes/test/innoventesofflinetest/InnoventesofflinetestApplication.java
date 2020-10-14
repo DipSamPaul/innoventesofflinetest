cc51f5ae2686d35c9a53d56a5717859ce3960e63
samyadeep <samyadeep.paul@quinta.co.in>
2020-10-14 20:19:22 +0530

    first commit with spring boot repo

---
+++ innoventesofflinetest/src/main/java/com/innoventes/test/innoventesofflinetest/InnoventesofflinetestApplication.java
@@ -0,0 +1,13 @@
+package com.innoventes.test.innoventesofflinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnoventesofflinetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InnoventesofflinetestApplication.class, args);
+	}
+
+}
---
+++ innoventesofflinetest/src/test/java/com/innoventes/test/innoventesofflinetest/InnoventesofflinetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innoventes.test.innoventesofflinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnoventesofflinetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
